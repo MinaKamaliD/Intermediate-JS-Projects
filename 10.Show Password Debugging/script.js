9c07de5acfaf44f5e7b414d7ba2b456adf07296d
Your Name <you@example.com>
2022-08-06 18:35:11 +0430

    script.js debugged

---
+++ 10.Show Password Debugging/script.js
@@ -0,0 +1,24 @@
+﻿// Functions for select elems with id & class
+var $ = document;
+function _id(id_name) {
+  return $.getElementById(id_name);
+}
+
+function _class(class_name) {
+  return $.getElementsByClassName(class_name);
+}
+
+// Select Elems
+var togglePassword = document.querySelector(".toggle-password");
+var passwordField = document.getElementById("password-field");
+
+// Fire click event on eye icon
+togglePassword.addEventListener("click", function () {
+  if (passwordField.type == "text") {
+    passwordField.type = "password";
+    togglePassword.classList.remove("active");
+  } else {
+    passwordField.type = "text";
+    togglePassword.classList.add("active");
+  }
+});
