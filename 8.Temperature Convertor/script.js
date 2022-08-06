c79e088cd13ee851db1b5cd2d7f93156cfc5d713
Your Name <you@example.com>
2022-08-06 15:28:12 +0430

    change button finished

--- 8.Temperature Convertor/script.js
+++ 8.Temperature Convertor/script.js
@@ -6,3 +6,26 @@ let result = document.querySelector(".result");
 let convertButton = document.querySelector(".convertButton");
 let resetButton = document.querySelector(".resetButton");
 let changeButton = document.querySelector(".changeButton");
+let scale = "c";
+
+function changeScale() {
+  if (scale === "c") {
+    title.innerHTML = "Temperature Convertor | °F to °C";
+    scale = "f";
+
+    c.innerHTML = "°F";
+    f.innerHTML = "°C";
+
+    tempInput.placeholder = "°F";
+  } else {
+    title.innerHTML = "Temperature Convertor | °C to °F";
+    scale = "c";
+
+    c.innerHTML = "°C";
+    f.innerHTML = "°F";
+
+    tempInput.placeholder = "°C";
+  }
+}
+
+changeButton.addEventListener("click", changeScale);
