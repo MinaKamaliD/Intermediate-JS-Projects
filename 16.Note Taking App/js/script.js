e090491f0f3f330de79d8bca834310f1ef071598
Your Name <you@example.com>
2022-08-07 11:10:20 +0430

    variables created

---
+++ 16.Note Taking App/js/script.js
@@ -0,0 +1,4 @@
+let input = document.querySelector(".form-control");
+let colors = document.querySelectorAll(".color-box");
+let saveBtn = document.getElementById("btn-save");
+let deleteBtn = document.getElementById("btn-delete");
