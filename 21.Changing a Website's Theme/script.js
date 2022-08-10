cdb5415766942b0edd69ae29453254a0d2424b7f
Your Name <you@example.com>
2022-08-10 07:22:51 +0430

    script.js modified

--- 21.Changing a Website's Theme/script.js
+++ 21.Changing a Website's Theme/script.js
@@ -2,6 +2,20 @@ let switchButton = document.querySelector(".switch");
 
 function changeTheme() {
   document.body.classList.toggle("dark");
+
+  if (document.body.className.includes("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+}
+
+function saveTheme() {
+  let localStorageTheme = localStorage.getItem("theme");
+  if (localStorageTheme === "dark") {
+    document.body.classList.add("dark");
+  }
 }
 
 switchButton.addEventListener("click", changeTheme);
+window.addEventListener("load", saveTheme);
