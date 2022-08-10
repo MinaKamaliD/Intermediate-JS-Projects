91659156c9ee98fc8922a27deaff77afc7978ed1
Your Name <you@example.com>
2022-08-10 15:16:23 +0430

    variables created

---
+++ 22.To-Do List with Local Storage/script.js
@@ -0,0 +1,4 @@
+let input = document.querySelector(".form-control");
+let addButton = document.querySelector("#addButton");
+let clearButton = document.querySelector("#clearButton");
+let todoUl = document.getElementById("todoList");
