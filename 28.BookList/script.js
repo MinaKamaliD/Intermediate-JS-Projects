e04beccafc6a646ab67a599bbe4d1ec924ba6dea
Your Name <you@example.com>
2022-08-16 21:17:28 +0430

    variables created

---
+++ 28.BookList/script.js
@@ -0,0 +1,5 @@
+let title = document.getElementById("title");
+let author = document.getElementById("author");
+let year = document.getElementById("year");
+let btn = document.querySelector(".btn");
+let bookContainer = document.querySelector(".table");
