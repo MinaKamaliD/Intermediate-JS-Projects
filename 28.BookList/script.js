ede29a024ec1e65e73305b1ac14cdfd836912a53
Your Name <you@example.com>
2022-08-17 14:10:27 +0430

    first part finished

--- 28.BookList/script.js
+++ 28.BookList/script.js
@@ -1,5 +1,34 @@
-let title = document.getElementById("title");
-let author = document.getElementById("author");
-let year = document.getElementById("year");
+let titleInput = document.getElementById("title");
+let authorInput = document.getElementById("author");
+let yearInput = document.getElementById("year");
 let btn = document.querySelector(".btn");
 let bookContainer = document.querySelector(".table");
+let bookArray = [];
+
+function addBook(event) {
+  event.preventDefault();
+
+  let newBookObject = {
+    id: bookArray.length + 1,
+    title: titleInput.value,
+    author: authorInput.value,
+    year: yearInput.value,
+  };
+
+  bookArray.push(newBookObject);
+
+  setIntoLocalStorage(bookArray);
+  makeInputsEmpty();
+}
+
+function setIntoLocalStorage(allBooksArray) {
+  localStorage.setItem("books", JSON.stringify(allBooksArray));
+}
+
+function makeInputsEmpty() {
+  titleInput.value = "";
+  authorInput.value = "";
+  yearInput.value = "";
+}
+
+btn.addEventListener("click", addBook);
