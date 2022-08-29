0ba2a168659a6311c80c5fa373b266982afc72bb
Your Name <you@example.com>
2022-08-29 07:35:51 +0430

    files added

---
+++ 36.Find a User By ID/script.js
@@ -0,0 +1,11 @@
+// DOM => Document Object Model
+// BOM => Browser Object Model
+
+let users = [
+  { id: 1, name: "Amin", age: 21 },
+  { id: 2, name: "Amir", age: 23 },
+  { id: 3, name: "Ali", age: 24 },
+  { id: 4, name: "Hasan", age: 19 },
+  { id: 5, name: "Babak", age: 29 },
+  { id: 6, name: "Reza", age: 32 },
+];
