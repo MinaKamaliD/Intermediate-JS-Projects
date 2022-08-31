623b717f3325cf275e8b5f41f9c5a256c75b3462
Your Name <you@example.com>
2022-08-31 07:31:58 +0430

    script.js modified

--- 36.Find a User By ID/script.js
+++ 36.Find a User By ID/script.js
@@ -2,10 +2,21 @@
 // BOM => Browser Object Model
 
 let users = [
-  { id: 1, name: "Amin", age: 21 },
-  { id: 2, name: "Amir", age: 23 },
+  { id: 1, name: "Hoda", age: 33 },
+  { id: 2, name: "Reza", age: 39 },
   { id: 3, name: "Ali", age: 24 },
   { id: 4, name: "Hasan", age: 19 },
-  { id: 5, name: "Babak", age: 29 },
-  { id: 6, name: "Reza", age: 32 },
+  { id: 5, name: "Kurosh", age: 33 },
+  { id: 6, name: "Mina", age: 32 },
 ];
+
+let userInfo = document.querySelector("h1");
+let locationSearch = new URLSearchParams(location.search);
+let userId = locationSearch.get("id");
+
+let mainUser = users.find(function (user) {
+  return user.id == userId;
+});
+if (mainUser) {
+  userInfo.innerHTML = "Name:" + mainUser.name + "| Age:" + mainUser.age;
+} else userInfo.innerHTML = "Use Is Not Defined";
